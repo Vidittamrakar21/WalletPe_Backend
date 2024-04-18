cda84221d684d3cf8a66aad6f7192df2536e4148
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-04-19 01:40:20 +0530

    up

--- dist/index.js
+++ dist/index.js
@@ -15,12 +15,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 //@ts-ignore
 const test_1 = __importDefault(require("./routes/test"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use((0, morgan_1.default)('tiny'));
+main().catch(err => console.log(err));
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield mongoose_1.default.connect(`mongodb+srv://vidit:${process.env.dbpass}@cluster0.2votuvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+        console.log("Database Connected");
+    });
+}
 app.use('/check', test_1.default);
 app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express, {Request, Response} from 'express';
 import morgan from 'morgan';
 import cors from 'cors'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 //@ts-ignore
 import apirouter from './routes/test'
 
@@ -9,6 +12,14 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(`mongodb+srv://vidit:${process.env.dbpass}@cluster0.2votuvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    console.log("Database Connected");
+  }
+
 
 app.use('/check', apirouter);
 
